68af57c7199d13f36eac15d5d443d7e039d2bb07
hugo de araujo <hugo.saraujo@tutanota.com>
2024-04-27 16:18:25 -0300

    Tratamento de erros para casos de insucesso do consumo da API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,19 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
-
+import java.util.Scanner
 
 fun main() {
+    val leitura = Scanner(System.`in`)
+
+    print("Digite o código de um jogo para buscar: ")
+    val buscaId = leitura.nextLine()
+
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$buscaId"
+
     val client = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(endereco))
         .build()
 
     val response = client
@@ -18,9 +25,29 @@ fun main() {
 
     val gson = Gson()
     val infoJogo = gson.fromJson(json, InfoJogo::class.java)
-    val jogo1 = Jogo(
-        infoJogo.info.title,
-        infoJogo.info.thumb)
+    var meuJogo:Jogo? = null
 
+    val resultadoBusca = runCatching {
+        meuJogo = Jogo(
+            infoJogo.info.title,
+            infoJogo.info.thumb)
+    }
 
+    resultadoBusca.onFailure {
+        println("Jogo inexistente. Tente buscar por outro jogo")
+    }
+
+    resultadoBusca.onSuccess {
+        print("Deseja criar uma descrição personalizada? (S/N)")
+        val opcao = leitura.nextLine()
+        if(opcao.equals("s", true)){
+            print("Insira a descrição personalizada: ")
+            val descricaoPersonalizada = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizada
+        } else {
+            meuJogo?.descricao = meuJogo?.titulo
+        }
+    }
+
+    println(meuJogo)
 }
