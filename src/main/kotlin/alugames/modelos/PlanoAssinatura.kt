f1023d6a1d3b2c559044a0cbe1dd309f61e99cf1
hugo de araujo <hugo.saraujo@tutanota.com>
2024-05-14 15:50:08 -0300

    Criação de classe reputação e implementação de descontros proporcionais para cada plano.

--- src/main/kotlin/alugames/modelos/PlanoAssinatura.kt
+++ src/main/kotlin/alugames/modelos/PlanoAssinatura.kt
@@ -4,12 +4,17 @@ class PlanoAssinatura(
     tipo: String,
     val mensalidade: Double): Plano(tipo) {
     private val jogosIncluidos = 3
+    private val percentualDeDescontoReputacao = 0.15
     override fun obterValor(aluguel: Aluguel): Double {
         val totalJogosNoMes = aluguel.gamer.jogosDoMes(aluguel.periodo.dataInicial.monthValue).size
         return if (totalJogosNoMes < jogosIncluidos) {
             0.0
         } else {
-            super.obterValor(aluguel)
+            var valorOriginal = super.obterValor(aluguel)
+            if(aluguel.gamer.media > 8){
+                valorOriginal -= valorOriginal * percentualDeDescontoReputacao
+            }
+            return valorOriginal
         }
     }
 
